2393e1ff776c750d3f0c8e79ce470806bae12c33
Mehmet Akif Seçkin <121381437+k4zmo55@users.noreply.github.com>
2023-10-02 22:24:09 +0300

    Add project files.

---
+++ Persistence/DesignTimeDbContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Persistence.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence
+{
+	public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<APIDbContext>
+	{
+		public APIDbContext CreateDbContext(string[] args)
+		{
+			DbContextOptionsBuilder<APIDbContext> dbContextOptionsBuilder = new();
+			dbContextOptionsBuilder.UseSqlServer(Configuration.ConnectionString);
+			return new(dbContextOptionsBuilder.Options);
+		}
+	}
+}
---
+++ Persistence/ServiceRegistiration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Persistence.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence
+{
+	public static class ServiceRegistiration
+	{
+		public static void AddPersistenceService(this IServiceCollection services)
+		{
+			services.AddDbContext<APIDbContext>(options => options.UseSqlServer(Configuration.ConnectionString));		}
+	}
+}
